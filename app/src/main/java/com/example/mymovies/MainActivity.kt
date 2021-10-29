78a74d9cbc5598e32e78f237dc781a9b24a46117
Mario Manzano <mario.manzano@experis.es>
2021-10-29 14:35:27 +0200

    Added MediaList, MediaListItem and Greeting Composables.

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.mymovies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.mymovies.MediaItem.*
 import com.example.mymovies.ui.theme.MyMoviesTheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +36,76 @@ class MainActivity : ComponentActivity() {
             MyMoviesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MediaList()
                 }
             }
         }
     }
 }
 
+@ExperimentalFoundationApi
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MediaList() {
+    LazyVerticalGrid(
+        contentPadding = PaddingValues(2.dp),
+        cells = GridCells.Adaptive(150.dp)
+    ) {
+        items(getMedia()) { item ->
+            MediaListItem(item, Modifier.padding(2.dp))
+        }
+    }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MyMoviesTheme {
-        Greeting("Android")
+fun MediaListItem(item: MediaItem, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        Box(modifier = Modifier
+            .height(200.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center){
+            Image(
+                painter = rememberImagePainter(
+                    data = item.thumb),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            if (item.type == Type.VIDEO) {
+                Icon(
+                    imageVector = Icons.Default.PlayCircleOutline,
+                    contentDescription = null,
+                    modifier = Modifier.size(92.dp),
+                    tint = Color.White
+                )
+            }
+        }
+        Box(contentAlignment= Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colors.secondary)
+                .padding(16.dp)) {
+            Text(text = item.title,
+            style = MaterialTheme.typography.h6)
+        }
     }
+}
+
+//@Preview(showBackground = true, widthDp = 200, heightDp = 100)
+@Composable
+fun ButtonText() {
+    Box(modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.Center){
+        Text(
+            text = "Hello World"
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
 }
