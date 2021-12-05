3a236cb3ff9c752f3c416099115151c548b822d6
Mario Manzano <mario.manzano@experis.es>
2021-12-05 19:29:10 +0100

    Added Theme to MainScreen.

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import coil.annotation.ExperimentalCoilApi
 import com.example.mymovies.ui.navigation.Navigation
+import com.example.mymovies.ui.theme.MyMoviesTheme
+import com.example.mymovies.ui.theme.Shapes
+import com.example.mymovies.ui.theme.Typography
 
 @ExperimentalFoundationApi
 @ExperimentalCoilApi
@@ -14,7 +19,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+           MyMoviesTheme {
+                Navigation()
+            }
         }
     }
 }
